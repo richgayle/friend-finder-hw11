2beed0e49e4011a17d828a08c00838496f603ff9
richgayle <rich002k@yahoo.com>
2020-01-01 22:51:51 -0500

    app routing

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+var friends = [
+  {
+    name: "Will Smith",
+    photo:
+      "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/12/pressonly_smith_onestrangerock_03_-_h_2017.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+  },
+  {
+    name: "Barack Obama",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg",
+    scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+  },
+  {
+    name: "Kevin Hart",
+    photo:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hart-gettyimages-1062194608mod-1544461779.jpg?crop=1xw:1xh;center,top&resize=768:*",
+    scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+  },
+  {
+    name: "Danai Gurira",
+    photo:
+      "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2019/11/danai_gurira_headshot_vertical-kwaku_alston-h_2019.jpg",
+    scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+  },
+  {
+    name: "Chadwick Boseman",
+    photo:
+      "https://indiewire.com/wp-content/uploads/2019/02/shutterstock_10112916td.jpg?w=780",
+    scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+  },
+  {
+    name: "Lupita Nyong'o",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/3/34/SXSW_2019_4_%2847282558132%29_%28cropped%29.jpg",
+    scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+  },
+  {
+    name: "Usain Bolt",
+    photo:
+      "https://www.gazetaesportiva.com/wp-content/uploads/imagem/2018/04/12/000_13Y734.jpg",
+    scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+  }
+];
+module.exports = friends;
